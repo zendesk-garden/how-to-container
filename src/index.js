3785fd74cb042e9ee3792c8c02140eab12e165ea
Ryan Seddon <seddon.ryan@gmail.com>
2019-06-05 12:49:16 +1000

    tooltip example suppressing onMouseEnter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom';
+import { useTooltip } from '@zendeskgarden/container-tooltip';
+
+import './styles.css';
 
 function App() {
-  return <div>Nothing to see here yet</div>;
+  const tooltipRef = useRef(null);
+
+  const { isVisible, getTooltipProps, getTriggerProps } = useTooltip({
+    tooltipRef
+  });
+
+  const styles = {
+    visibility: isVisible ? 'visible' : 'hidden',
+    background: '#1f73b7',
+    padding: '10px',
+    margin: '6px 0',
+    color: '#fff'
+  };
+
+  return (
+    <div>
+      <button {...getTriggerProps({ onMouseEnter: e => e.preventDefault() })}>
+        Focus or click
+      </button>
+      <div
+        {...getTooltipProps({
+          ref: tooltipRef,
+          style: styles
+        })}
+      >
+        Tooltip
+      </div>
+    </div>
+  );
 }
 
 const rootElement = document.getElementById('root');
